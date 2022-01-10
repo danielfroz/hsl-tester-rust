a4ca2a2f750c17227f4ba342a9d4f1447d697319
Daniel Froz <daniel.froz@gmail.com>
2022-01-10 19:28:54 -0300

    log files ignored

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 // use std::fs::File;
 use std::net::UdpSocket;
 use std::str;
+use std::env;
+use std::fs::File;
+use std::io::Write;
 use chrono::prelude::*;
 
+fn usage() {
+    eprintln!("usage: hsl <address:port> <filename|->");
+}
+
 fn main() -> std::io::Result<()> {
-    let socket = UdpSocket::bind("0.0.0.0:11111")?;
-    println!("Running server....");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        usage();
+        return Ok(());
+    }
+
+    let address = &args[1];
+    let filename = &args[2];
+    let output = match filename {
+        _ if filename == "-" => "STDOUT",
+        _ => filename
+    };
+
+    let mut file: Option<File> = None;
+    if !filename.eq("-") {
+        file = Some(File::create(filename).unwrap());
+    }
+
+    let socket = UdpSocket::bind(address)?;
+    println!("Running server on address {} with output to {}", address, output);
+
     let mut buf = [0; 8 * 1024];
     loop {
         let (amt, src) = socket.recv_from(&mut buf)?;
@@ -14,9 +41,12 @@ fn main() -> std::io::Result<()> {
             Ok(v) => v,
             Err(e) => panic!("Invalid UTF-8 sequence {}", e)
         };
-
         let now = Utc::now();
-        print!("{} client[{}] - {}", now.to_string(), src, str)
+        match file {
+            None => print!("{} client[{}] - {}", now.to_string(), src, str),
+            _ => {
+                write!(file.as_ref().unwrap(), "{} client[{}] - {}", now.to_string(), src, str)?;
+            }
+        }
     }
-    // Ok(())
 }
