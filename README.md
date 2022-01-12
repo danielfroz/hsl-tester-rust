210a997b09151070d9c3770fae52516b22224ff5
Daniel Froz <daniel.froz@gmail.com>
2022-01-12 17:19:02 -0300

    attempt to add LF at the end of buf; if not already addded

--- README.md
+++ README.md
@@ -5,17 +5,16 @@ This is still a working progress...
 
 # How to use it?
 
-Deploy the package or build from source ... once you have the ```hsl-tester``` on your target linux or macos
-then just use the following commands:
+Deploy the package or build from source ... use the correct release / package (linux debian/centos or macos).
+
+Once you have the binary ```hsl-tester``` on your server... just use the following commands:
 
 ```
 $ ./hsl-tester 0.0.0.0:11111 -
 ```
-Note: command above is instructing to listen to UDP on all interfaces on port 11111. 
-There is no real need for TCP implementation. So far, only UDP option is available.
+Note that the command above is instructing to listen to UDP on all interfaces on port 11111. There is no real need for TCP implementation. So far, only UDP option is available.
 
-It will print all the HSL messages to the STDOUT. If you want to inspect the messages, just either use tee
-or instead of using dash as last argument, add the file path / filename...
+It will print all the HSL messages to the STDOUT. If you want to inspect the messages, just either use tee or instead of using dash as last argument, add the file path / filename...
 
 ```
 $ ./hsl-tester 0.0.0.0:11111 output.log
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use std::net::UdpSocket;
 use std::str;
 use std::env;
 use std::fs::File;
-use std::io::Write;
+use std::io::{self, Write};
 use chrono::prelude::*;
+
+const MAXBUF: usize = 8 * 1024;
 
 fn usage() {
     eprintln!("usage: hsl <address:port> <filename|->");
@@ -32,18 +34,28 @@ fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind(address)?;
     println!("Running server on address {} with output to {}", address, output);
 
-    let mut buf = [0; 8 * 1024];
     loop {
-        let (amt, src) = socket.recv_from(&mut buf)?;
-        let buf = &mut buf[..amt];
+        let mut buf = [0; MAXBUF];
+        let (mut bytes_read, src) = socket.recv_from(&mut buf)?;
+        if bytes_read < MAXBUF && buf[bytes_read] != '\n' as u8 {
+            buf[bytes_read] = '\n' as u8;
+            bytes_read += 1;
+        }
+        let buf = &mut buf[..bytes_read];
         let str = match str::from_utf8(buf) {
             Ok(v) => v,
             Err(e) => panic!("Invalid UTF-8 sequence {}", e)
         };
         let now = Utc::now();
         match file {
-            None => print!("{} client[{}] - {}", now.to_string(), src, str),
-            _ => write!(file.as_ref().unwrap(), "{} client[{}] - {}", now.to_string(), src, str)?
+            None => {
+                print!("{} client[{}] - {}", now.to_string(), src, str);
+                io::stdout().flush().unwrap();
+            },
+            _ => {
+                write!(file.as_ref().unwrap(), "{} client[{}] - {}", now.to_string(), src, str)?;
+                // sync_all();
+            }
         }
     }
 }
